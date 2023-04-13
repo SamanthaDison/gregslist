3f52a9bbfc7184fff083f9d9321616beb2b0cdf3
SamanthaDison <samnicdis@gmail.com>
2023-04-13 11:43:12 -0600

    fixed jeremy's broken img

--- app/AppState.js
+++ app/AppState.js
@@ -9,6 +9,7 @@ class AppState extends EventEmitter {
   values = loadState('values', [Value])
 
   cars = [
+    new Car({ make: 'Toyota', model: 'Camry', img: 'https://s1.cdn.autoevolution.com/images/news/gallery/mr-regular-reviews-the-seven-seat-1993-toyota-camry-wagon-v6_8.jpg', year: 1993, price: 1000, description: "ole' reliable", color: '#45e7f0' }),
     new Car({ make: 'Toyota', model: 'Camry', img: 'https://images.unsplash.com/photo-1588440983028-d53e24fa96cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', year: 1993, price: 1000, description: "ole' reliable", color: '#45e7f0' })
   ]
 
